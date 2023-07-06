d6740470065369625d6d8249a2f42fb772dec822
Artur <129121513+ArturMaia@users.noreply.github.com>
2023-07-06 15:57:45 -0300

    Update SOLO.R
    coleção de mapas hipsométrico com malhas territoriais:
    mundial, continental, país, estado e município.

--- SOLO.R
+++ SOLO.R
@@ -3,53 +3,70 @@ library(ggplot2)
 library(maptools)
 library(raster)
 library(rgdal)
+library(sf)
 library(rgeos)
 library(RColorBrewer)
 library(dplyr)
 library(geodata)
 library(geobr)
+library(elevatr)
 
-#MUNDIAL
-terra<-getData('worldclim', var='alt', res=10)
+#PALETA DE CORES
 cores <-c('#bcd2a4','#89d2a4','#28a77e',
           '#90b262','#ddb747','#fecf5b',
           '#da9248','#b75554','#ad7562',
           '#b8a29a','#9f9e98')
 
-cores <- colorRampPalette(cores)(250)
 
+#MUNDIAL
+terra<-getData('worldclim', var='alt', res=10)
+cores <- colorRampPalette(cores)(250)
 plot(terra,col=cores,
      legend.width= 1.5,  axis.args=list(cex.axis=0.6),
      legend.args=list(text="Altitude (mt)",
-                      side=3, line=0.5, cex=0.8))
+                      side=3, line=0.5, cex=0.8))+title('Hipsometria mundial')
+
 
 #CONTINENTAL
 continente<-ne_countries(continent  = 'south america' , returnclass = 'sf')
 altitude<- getData('worldclim', var='alt' , res=10)
 sam<-crop(altitude,continente)
-
 cores <- colorRampPalette(cores)(250)
-
 plot(sam,col=cores,
      legend.width= 1.5,  axis.args=list(cex.axis=0.6),
      legend.args=list(text="Altitude (mt)",
-                      side=3, line=0.5, cex=0.8))
+                      side=3, line=0.5, cex=0.8))+title('Hipsometria da América do Sul')
+
+
 #PAÍS
-mada<-getData('alt', country='Madagascar', mask=TRUE) 
-
+br<-getData('alt', country='Brazil', mask=TRUE) 
 cores <- colorRampPalette(cores)(250)
-
-plot(mada,col=cores,
+plot(br,col=cores,
      legend.width= 1.5,  axis.args=list(cex.axis=0.6),
      legend.args=list(text="Altitude (mt)",
-                      side=3, line=0.5, cex=0.8))
-#ESTADO
-pi<-read_state(code_state = "PI", year= 2010)
-topo<-get_elev_raster(pi,z=8,clip ='location')
-cores <- colorRampPalette(cores)(250)
+                      side=3, line=0.5, cex=0.6))+title('Hipsometria do Brasil')
 
-plot(pi$geom)+
+#ESTADO
+pa<-read_state(code_state = "PA", year= 2010)
+topo<-get_elev_raster(pa,z=8,clip ='location')
+cores <- colorRampPalette(cores)(250)
+plot(pa$geom)+
   plot(topo,add=T,col=cores,legend.width= 1.5,axis.args=list(cex.axis=0.6),
        legend.args=list(text="Altitude (mt)",
-                        side=3, line=0.5, cex=0.8),title('Hipsometria do Estado do Piauí'))
+                        side=3, line=0.5, cex=0.8))+title('Hipsometria do Estado do Pará')
+
+
+#MUNICIPIO
+file.choose()
+img<-raster("C:\\Users\\luiza\\OneDrive\\Documentos\\raster\\ananin.tif")
+ananindeua<-mun<-read_municipality(code_muni = 1500800)
+recortado<-crop(img,ananindeua)
+cores <- colorRampPalette(cores)(250)
+plot(recortado,col=cores,legend.width= 1.5,axis.args=list(cex.axis=0.6),
+       legend.args=list(text="Altitude (mt)",
+                        side=3, line=0.5, cex=0.8))+
+  plot(ananindeua$geom,add=T)+title('Hipsometria de ananindeua')
+
+
+
 
